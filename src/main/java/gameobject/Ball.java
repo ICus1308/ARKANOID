eb5b4a9dc496d6d53ec5a0be506ba22114efc3a8
BinhVuuuuu <vungocthienbinh@gmail.com>
2025-10-06 16:10:21 +0700

    [fix]: repair Ball and Paddle logic

---
+++ src/main/java/gameobject/Ball.java
@@ -0,0 +1,81 @@
+package arkanoid.arkanoid.gameobject;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import java.util.Random;
+import arkanoid.arkanoid.gameconfig.GameConfig.WallSideType;
+
+public class Ball extends GameObject {
+    private final Circle node;
+    private double vx, vy;
+    public double speed;
+    private final double radius;
+    private boolean stuck = true;
+
+    public Ball(double x, double y, double radius, double speed) {
+        super(x - radius, y - radius, radius * 2, radius * 2);
+        this.radius = radius;
+        this.speed = speed;
+        this.node = new Circle(radius, Color.SALMON);
+        reset(x, y);
+    }
+
+    @Override
+    public javafx.scene.Node getNode() { return node; }
+
+    @Override
+    public void setX(double x) { this.x = x; }
+    @Override
+    public void setY(double y) { this.y = y; }
+
+    public double getRadius() { return radius; }
+    public double getVx() { return vx; }
+    public double getVy() { return vy; }
+    public void setVx(double vx) { this.vx = vx; }
+    public void setVy(double vy) { this.vy = vy; }
+    public void setStuck(boolean stuck) { this.stuck = stuck; }
+
+    public void launch() {
+        vx = (new Random().nextBoolean() ? 1 : -1) * speed * 0.7;
+        vy = -speed;
+        stuck = false;
+    }
+
+    public void reset(double x, double y) {
+        setX(x - radius);
+        setY(y - radius);
+        stuck = true;
+        vx = 0;
+        vy = 0;
+        node.setCenterX(this.x + radius);
+        node.setCenterY(this.y + radius);
+    }
+
+    public void update(double tpf, Paddle paddle, double gameWidth, double gameHeight) {
+        if (stuck) {
+            setX(paddle.getX() + paddle.getWidth() / 2 - radius);
+            setY(paddle.getY() - height);
+        } else {
+            setX(x + vx * tpf * 60);
+            setY(y + vy * tpf * 60);
+        }
+        node.setCenterX(x + radius);
+        node.setCenterY(y + radius);
+    }
+
+    public void bounce(WallSideType wallSide) {
+        switch (wallSide) {
+            case NORTH:
+            case SOUTH:
+                vy *= -1;
+                break;
+            case EAST:
+            case WEST:
+                vx *= -1;
+                break;
+            case BOTTOM_HIT:
+            case HIT_OUT_OF_BOUNDS:
+                break;
+        }
+    }
+}
---
+++ src/main/java/gameobject/Paddle.java
@@ -0,0 +1,62 @@
+package arkanoid.arkanoid.gameobject;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import static arkanoid.arkanoid.gameconfig.GameConfig.*;
+import arkanoid.arkanoid.gameconfig.GameConfig;
+
+
+public class Paddle extends GameObject {
+    private final Rectangle node;
+    private double speed;
+    private boolean extended = false;
+    private double currentWidth;
+
+    public Paddle(double x, double y, double width, double height, double speed) {
+        super(x, y, width, height);
+        this.speed = speed;
+        this.currentWidth = width;
+        this.node = new Rectangle(width, height, Color.LIGHTSEAGREEN);
+        this.node.setArcWidth(10);
+        this.node.setArcHeight(10);
+        setX(x);
+        setY(y);
+    }
+
+    @Override
+    public javafx.scene.Node getNode() { return node; }
+
+    public void moveLeft(double tpf) {
+        double dx = speed * tpf * 60;
+        if (x > 0) {
+            setX(Math.max(0, x - dx));
+        }
+    }
+
+    public void moveRight(double tpf) {
+        double dx = speed * tpf * 60;
+        if (x + width < GAME_WIDTH) {
+            setX(Math.min(GAME_WIDTH - width, x + dx));
+        }
+    }
+
+    public void reset() {
+        setX(GAME_WIDTH / 2 - width / 2);
+        if (extended) {
+            this.width = 100;
+            this.node.setWidth(100);
+            extended = false;
+        }
+    }
+
+    public void applyPowerup(PowerupType type) {
+        if (type == GameConfig.PowerupType.EXTEND) {
+            if (!extended) {
+                currentWidth = this.width * 1.5;
+                node.setWidth(currentWidth);
+                this.width = currentWidth;
+                extended = true;
+            }
+        }
+    }
+}
